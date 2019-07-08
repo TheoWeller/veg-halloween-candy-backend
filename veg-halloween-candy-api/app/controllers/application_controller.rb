7ecfaab6f787fb50b87c474096054d317ad4ac46
Theo Weller <weller.theodor@gmail.com>
2019-07-08 11:47:32 -0400

    jwt token helper methods added to application controller

--- veg-halloween-candy-api/app/controllers/application_controller.rb
+++ veg-halloween-candy-api/app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+require 'jwt'
 class ApplicationController < ActionController::API
+  # def issue_token(payload)
+  #   JWT.encode(payload, ENV["SECRET_KEY"], 'HS256')
+  # end
+  #
+  # def decode_token
+  #   begin
+  #     JWT.decode(@token, ENV["SECRET_KEY"], true, {algorithm: 'HS256'}).first
+  #   rescue JWT::DecodeError
+  #     return {id: nil}
+  #   end
+  # end
+  #
+  # def token
+  #   request.headers["Authenticate"]
+  # end
+  #
+  # def id_from_token
+  #   @decoded = decode_token
+  #   if @decoded
+  #     @decoded["id"]
+  #   end
+  # end
 end
